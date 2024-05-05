b60b06ca6c5c5067eedd6356081ee2e651f89d6f
Daniel Burtsev <daniel.burtsev35033@gmail.com>
2024-05-05 17:07:36 +0300

    Turtle test

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from turtle import *
 
+Screen().setup(1000, 800)
+
+shape("turtle")
+showturtle()
+goto(0,0)
+goto(100, 0)
+penup()
+goto(100, 100)
+pendown()
+goto(200, 100)
+dot(200, "green")
+
+pensize(7)
+pencolor("blue")
+goto(200, -200)
+
+mainloop()
