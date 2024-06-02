de4a01d1c3a341c9708e46f94891f5ee2f4960d0
Daniel Burtsev <daniel.burtsev35033@gmail.com>
2024-06-02 16:56:20 +0300

    add click on letters and draw errors

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ def draw_square(topleftpoint, lenedge):
     draw_line(points[2], points[3])
     draw_line(points[3], points[0])
 
+def draw_error(num_error):
+    match num_error:
+        case 1:
+            draw_line((-400, -180), (-70, -180))
+        case 2:
+            draw_line((-100, -180), (-100, 280))
+        case 3:
+            draw_line((-100, 280), (-270, 280))
+        case 4:
+            draw_line((-270, 280), (-270, 220))
+        case 5:
+            draw_circle((-270, 220), 50)
+        case 6:
+            draw_line((-270, 120), (-270, -40))
+        case 7:
+            draw_line((-270, 100), (-330, 30))
+        case 8:
+            draw_line((-270, 100), (-210, 30))
+        case 9:
+            draw_line((-270, -40), (-330, -150))
+        case 10:
+            draw_line((-270, -40), (-210, -150))
+
 def draw_secret(word):
     lenedge = 100
     interval = 20
@@ -39,6 +62,7 @@ def draw_alphabet():
         if x >= 500:
             x = 100
             y -= fontSize * 2
+    print()
 
 def draw_line(point1, point2):
     penup()
@@ -55,6 +79,17 @@ def draw_circle(point1, radius):
     circle(-radius)
     penup()
     goto(point1[0], point1[1]-radius * 2)
+def get_letter_on_click(x,y):
+    global alphabetDict
+    for key, value in alphabetDict.items():
+        charX = value[0]
+        charY = value[1]
+        if abs(charX - x) < 50 and abs(charY - y) < 40:
+            print(key)
+            break
+
+
+
 
 Screen().setup(1000, 800)
 
@@ -63,16 +98,11 @@ shape("classic")
 showturtle()
 
 pensize(5)
-draw_line((-400, -180),(-70, -180))
-draw_line((-100, -180),(-100, 280))
-draw_line((-100, 280),(-270, 280))
-draw_line((-270, 280),(-270, 220))
-draw_circle((-270, 220), 50)
-draw_line((-270, 120),(-270, -40))
-draw_line((-270, 100),(-330, 30))
-draw_line((-270, 100),(-210, 30))
-draw_line((-270, -40),(-330, -150))
-draw_line((-270, -40),(-210, -150))
+draw_error(1)
 draw_secret("f")
 draw_alphabet()
+onscreenclick(get_letter_on_click)
+
+
+
 mainloop()
